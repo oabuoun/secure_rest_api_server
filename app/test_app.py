d51eea889fd1c3e624ceecc0069a38fbdbec5841
Dr. Osama Abu Oun <oabu@spartaglobal.com>
2021-10-07 15:31:05 +0100

    Pytest shouldn't be working

--- app/test_app.py
+++ app/test_app.py
@@ -4,7 +4,7 @@ def test_func_1():
     assert 1 == 1, "Error: 1 should be equal to 1"
 
 def test_func_2():
-    assert 2 == 2, "Error: 2 should be equal to 2"
+    assert 2 == 1, "Error: 2 should be equal to 2"
 
 def test_func_3():
     assert 3 == 3, "Error: 3 should be equal to 3"
